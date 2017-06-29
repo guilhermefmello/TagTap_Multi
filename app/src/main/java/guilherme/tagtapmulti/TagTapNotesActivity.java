b0cdb3c09b9558eaca559438a21ab64fca642f05
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2017-06-29 12:58:31 +0100

    Creating NDEF Message into NFC Tag in TagTap Notes

--- app/src/main/java/guilherme/tagtapmulti/TagTapNotesActivity.java
+++ app/src/main/java/guilherme/tagtapmulti/TagTapNotesActivity.java
@@ -22,6 +22,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class TagTapNotesActivity extends AppCompatActivity {
 
@@ -120,6 +121,27 @@ public class TagTapNotesActivity extends AppCompatActivity {
         ndef.writeNdefMessage(message);
         // Close the connection
         ndef.close();
+    }
+
+    // Creating NDEF Message to write into the NFC TAG
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
+        String lang       = "en";
+        byte[] textBytes  = text.getBytes();
+        byte[] langBytes  = lang.getBytes("US-ASCII");
+        int    langLength = langBytes.length;
+        int    textLength = textBytes.length;
+        byte[] payload    = new byte[1 + langLength + textLength];
+
+        // set status byte (see NDEF spec for actual bits)
+        payload[0] = (byte) langLength;
+
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1,              langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,  NdefRecord.RTD_TEXT,  new byte[0], payload);
+
+        return recordNFC;
     }
 
 
