a6f07e45bb11ea652161befb0cf67f5e22ab7ad8
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2017-06-29 13:57:34 +0100

    Action Bar TagTap

--- app/src/main/java/guilherme/tagtapmulti/TagTapActivity.java
+++ app/src/main/java/guilherme/tagtapmulti/TagTapActivity.java
@@ -1,13 +1,40 @@
 package guilherme.tagtapmulti;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TagTapActivity extends AppCompatActivity {
+
+    private static final String TAG = "NFCWriteTag";
+    private NfcAdapter mNfcAdapter;
+    private IntentFilter[] mWriteTagFilters;
+    private PendingIntent mNfcPendingIntent;
+    private boolean silent=false;
+    private boolean writeProtect = false;
+    private Context context;
+
+    private Button btnWrite;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_tap);
+
+        //Displaying Logo on Action Bar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.tagtap_logo_48);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
+
+
     }
 }
