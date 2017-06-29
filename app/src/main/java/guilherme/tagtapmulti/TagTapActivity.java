f4785926f8c784864d0b46f659cd1c3db30b8027
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2017-06-29 13:59:36 +0100

    Finding a TAG TagTap

--- app/src/main/java/guilherme/tagtapmulti/TagTapActivity.java
+++ app/src/main/java/guilherme/tagtapmulti/TagTapActivity.java
@@ -2,6 +2,7 @@ package guilherme.tagtapmulti;
 
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
@@ -33,6 +34,29 @@ public class TagTapActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.tagtap_logo_48);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
+
+        /*
+         * TRYING TO FIND A TAG:
+         * getting the default NFC adapter;
+         * Aren't creating multiple instances of the same application (using FLAG_ACTIVITY_SINGLE_TOP);
+         * Pending Intent performing action at a later time;
+         * Adding Flags to modify the default behavior of this activity;
+         * The IntentFilter advertising the types of intents that they can respond;
+         */
+
+        context = getApplicationContext();
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mNfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP), 0);
+        IntentFilter discovery=new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        // Intent filters for writing to a tag
+        mWriteTagFilters = new IntentFilter[] { discovery };
+
+
+        
 
 
 
