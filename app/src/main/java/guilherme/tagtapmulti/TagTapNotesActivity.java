e4912e797492af4f46ea942fd3277ac35d74254f
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2017-06-29 12:43:50 +0100

    Creating TagTap Notes Action Bar

--- app/src/main/java/guilherme/tagtapmulti/TagTapNotesActivity.java
+++ app/src/main/java/guilherme/tagtapmulti/TagTapNotesActivity.java
@@ -1,13 +1,32 @@
 package guilherme.tagtapmulti;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TagTapNotesActivity extends AppCompatActivity {
 
+    NfcAdapter adapter;
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    boolean writeMode;
+    Tag mytag;
+    Context ctx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_tap_notes);
+
+        //Displaying Logo on Action Bar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.tagtap_notes_menu_48x48);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
     }
 }
