85f79d1a48fb8edcb4f6086e12a95c987100fd5b
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2017-07-02 19:45:17 +0100

    Implementing Saving Notes' Subjects to Database

--- app/src/main/java/guilherme/tagtapmulti/SubjectActivity.java
+++ app/src/main/java/guilherme/tagtapmulti/SubjectActivity.java
@@ -1,12 +1,19 @@
 package guilherme.tagtapmulti;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SubjectActivity extends AppCompatActivity {
 
@@ -14,6 +21,10 @@ public class SubjectActivity extends AppCompatActivity {
     EditText editTextSubject;
     SeekBar seekBarRating;
     ListView ListViewSubject;
+    Button buttonAddSubject;
+
+    DatabaseReference databaseSubject;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class SubjectActivity extends AppCompatActivity {
         editTextSubject = (EditText) findViewById(R.id.editTextSubject);
         seekBarRating = (SeekBar) findViewById(R.id.seekBarRating);
         ListViewSubject = (ListView) findViewById(R.id.listViewSubject);
+        buttonAddSubject = (Button) findViewById(R.id.buttonAddSubject);
 
         Intent intent = getIntent();
 
@@ -32,5 +44,36 @@ public class SubjectActivity extends AppCompatActivity {
 
         textViewNotes.setText(notes);
 
+        databaseSubject = FirebaseDatabase.getInstance().getReference("Subjects").child(id);
+
+        buttonAddSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSubject();
+            }
+        });
+
     }
+
+    private void saveSubject(){
+
+        String subject = editTextSubject.getText().toString().trim();
+        int priority = seekBarRating.getProgress();
+
+        if(!TextUtils.isEmpty(subject)){
+            String subjectId = databaseSubject.push().getKey();
+
+            Subject subjects = new Subject(subjectId, subject, priority);
+            databaseSubject.child(subjectId).setValue(subjects);
+            Toast.makeText(this, "Subject Saved Successfully!", Toast.LENGTH_LONG).show();
+
+        }else{
+            Toast.makeText(this, "Subject Should Not Be Empty", Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
+
+
 }
